0f2d274f4a00cb88f156439c8b68ddff2ae6db1b
rsm4282@naver.com <rsm4282@naver.com>
2020-10-09 16:34:03 +0900

    Add 주식가격

---
+++ level_2/주식가격.py
@@ -0,0 +1,17 @@
+def solution(prices):
+    answer = []
+
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i+1,len(prices)):
+            count += 1
+            if prices[i] <= prices[j]:
+                continue
+            else:
+                break
+        answer.append(count)
+
+    return answer
+
+prices = [1,2,3,2,3]
+print(solution(prices))
